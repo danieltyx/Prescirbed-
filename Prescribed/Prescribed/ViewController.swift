d5b49a1e7c8cff07f183b43998ba673157f12f63
tirsong <81770238+tirsong@users.noreply.github.com>
2021-06-18 09:23:03 -0400

    Xcode Project

---
+++ Prescribed/Prescribed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prescribed
+//
+//  Created by Yanying Huo on 6/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
