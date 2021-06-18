3fa20ba71d6d38e69996c067c241417a0f77a386
danieltyx <danieltian.yx@gmail.com>
2021-06-18 21:41:27 +0800

    New File

---
+++ Prescribed/Prescribed/PrescribedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrescribedApp.swift
+//  Prescribed
+//
+//  Created by Daniel T. on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct PrescribedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
