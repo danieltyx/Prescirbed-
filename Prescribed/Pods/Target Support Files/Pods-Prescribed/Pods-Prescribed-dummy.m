ea963da554aa36cf1872df74ebd329c248b4a3a7
tirsong <81770238+tirsong@users.noreply.github.com>
2021-07-04 10:24:58 -0400

    Firebase Connection

---
+++ Prescribed/Pods/Target Support Files/Pods-Prescribed/Pods-Prescribed-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Prescribed : NSObject
+@end
+@implementation PodsDummy_Pods_Prescribed
+@end
---
+++ Prescribed/Prescribed/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prescribed
+//
+//  Created by Yanying Huo on 7/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
