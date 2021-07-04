6319347f50b0526abd96a56c23ee7d08e42aca8d
tirsong <81770238+tirsong@users.noreply.github.com>
2021-07-04 10:27:22 -0400

    User & User Info Models

---
+++ Prescribed/Prescribed/Model/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  Prescribed
+//
+//  Created by Yanying Huo on 7/4/21.
+//
+
+import Foundation
+import Foundation
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCoreDiagnostics
+import FirebaseAnalytics
+
+struct User {
+    
+    let uid: String
+    let email: String
+  
+    init(authData: FirebaseAuth.User)
+    {
+        self.email = authData.email!
+        self.uid = authData.uid
+    }
+  
+    init(uid: String, email: String)
+    {
+        self.uid = uid
+        self.email = email
+    }
+}
---
+++ Prescribed/Prescribed/Model/UserInfo.swift
@@ -0,0 +1,62 @@
+//
+//  UserInfo.swift
+//  Prescribed
+//
+//  Created by Yanying Huo on 7/4/21.
+//
+
+import Foundation
+import Foundation
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCoreDiagnostics
+import FirebaseAnalytics
+
+struct UserInfo {
+    
+    let ref: DatabaseReference!
+    let firstname: String
+    let lastname: String
+    //let uid: String
+    let email: String
+    
+  
+    init(firstname: String, lastname: String,  email: String)
+    {
+        self.ref = nil
+        self.firstname = firstname
+        self.lastname = lastname
+        //self.uid = uid
+        self.email = email
+    }
+    
+    init?(snapshot: DataSnapshot) {
+      guard
+        let value = snapshot.value as? [String: AnyObject],
+        let firstname = value["firstname"] as? String,
+        let lastname = value["lastname"] as? String,
+        let email = value["email"] as? String
+        else {
+        return nil
+      }
+      
+      self.ref = snapshot.ref
+        self.firstname = firstname
+        self.lastname = lastname
+        //self.uid = uid
+        self.email = email
+    }
+    
+    func toAnyObject() -> Any
+    {
+        return [
+          "firstname": firstname,
+            "lastname": lastname,
+            "email": email
+        ]
+    }
+    
+    
+}
