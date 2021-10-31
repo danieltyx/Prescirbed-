a36054f04c0e7310ad50b486bf84e8eae29ba780
tirsong <81770238+tirsong@users.noreply.github.com>
2021-10-31 12:09:04 -0400

    chatBot

--- Prescribed/Prescribed/Controller/chatBotViewController.swift
+++ Prescribed/Prescribed/Controller/chatBotViewController.swift
@@ -6,7 +6,27 @@
 //
 
 import UIKit
-
-class chatBotViewController: UIViewController {
+import WebKit
+class chatBotViewController: UIViewController, WKNavigationDelegate, WKUIDelegate,
+                             UISearchBarDelegate
+{
+    var webView: WKWebView!
+    let screenSize = UIScreen.main.bounds
+    
+    override func loadView()
+    {
+        let webConfiguration = WKWebViewConfiguration()
+        self.webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        self.webView.allowsBackForwardNavigationGestures = true
+        self.view = self.webView
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let url = URL(string: "https://cac2021-bot-1.yuxuantian.repl.co/")
+        let request = URLRequest(url: url!)
+        self.webView.load(request)
+    }
 
 }
